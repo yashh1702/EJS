bc3146c6ca9b2e12238637938b337f7c6c28c201
yashh1702 <yagrawal944@gmail.com>
2025-05-02 13:44:44 +0530

    CRUD operations

---
+++ Cards/models/user.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/testapp1");
+
+const userSchema = mongoose.Schema({
+    imageUrl: String,
+    email: String,
+    name: String,
+})
+
+module.exports = mongoose.model('user',userSchema)

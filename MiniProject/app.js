650adb5debd23902619d78a464d99b6c14e76860
yashh1702 <yagrawal944@gmail.com>
2025-05-07 23:41:01 +0530

    update profile picture using multer disk storage

--- MiniProject/app.js
+++ MiniProject/app.js
@@ -7,38 +7,18 @@ const bcrypt = require('bcrypt')
 const jwt = require("jsonwebtoken")
 const crypto = require('crypto');
 const path = require('path')
-const multer = require('multer')
+const upload = require('./config/multerconfig.js')
+
 
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"public")))
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/images/uploads')
-    },
-    filename: function (req, file, cb) {
-      crypto.randomBytes(12,function(err,bytes){
-        const fn = bytes.toString("hex") + path.extname(file.originalname)
-        cb(null, fn)
-      })
-      
-    }
-})
-  
-const upload = multer({ storage: storage })
 
 app.get("/",(req,res)=>{
     res.render("index")
-})
-
-app.get("/test",(req,res)=>{
-    res.render("test")
-})
-
-app.post("/upload", upload.single("image") ,(req,res)=>{
-     console.log(req.file);
 })
 
 app.get("/login",(req,res)=>{
@@ -73,6 +53,18 @@ app.get("/edit/:id",isLoggedIn,async (req,res)=> {
 
     res.render("edit",{post})
 });
+
+app.get("/profile/upload",(req,res)=>{
+    res.render("profileUpload")
+})
+
+app.post("/upload", isLoggedIn,  upload.single("image") ,async (req,res)=>{
+    let user = await userModel.findOne({email:req.user.email});
+    user.profilePic = req.file.filename;
+    await user.save()
+    res.redirect("/profile");
+})
+
 
 app.post("/update/:id",isLoggedIn,async (req,res)=> {
     let post = await postModel.findOneAndUpdate({_id:req.params.id},{content: req.body.content})
